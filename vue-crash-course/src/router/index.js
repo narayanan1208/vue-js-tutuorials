a9685482a1af555527e3da89379d7c7a3cf2b743
narayanan1208 <narayanan6647@gmail.com>
2025-08-12 14:13:54 +0530

    Added add job route

--- vue-crash-course/src/router/index.js
+++ vue-crash-course/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobView from "@/views/JobView.vue";
 import JobsView from "@/views/JobsView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
@@ -21,6 +22,11 @@ const router = createRouter({
       path: "/jobs/:id",
       name: "job",
       component: JobView,
+    },
+    {
+      path: "/jobs/add",
+      name: "add-job",
+      component: AddJobView,
     },
     {
       path: "/:catchAll(.*)",
