a5fdff1899cb092d4b93979b8d87293dc8a1c1af
narayanan1208 <narayanan6647@gmail.com>
2025-08-14 01:15:32 +0530

    Added edit view in main

--- vue-crash-course/src/router/index.js
+++ vue-crash-course/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "@/views/HomeView.vue";
 import JobView from "@/views/JobView.vue";
 import JobsView from "@/views/JobsView.vue";
 import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
@@ -27,6 +28,11 @@ const router = createRouter({
       path: "/jobs/add",
       name: "add-job",
       component: AddJobView,
+    },
+    {
+      path: "/jobs/edit/:id",
+      name: "edit-job",
+      component: EditJobView,
     },
     {
       path: "/:catchAll(.*)",
